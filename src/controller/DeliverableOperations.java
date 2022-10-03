9038786babf1f365d4d4c58ddef03413c5e28abc
haringpula <kingredpula31@gmail.com>
2022-10-04 01:15:46 +0800

    Implemented deliverable operations CRUD from db

---
+++ src/controller/DeliverableOperations.java
@@ -0,0 +1,135 @@
+package controller;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Deliverable;
+
+public class DeliverableOperations extends DatabaseConnection {
+
+    /**
+     * @param deliverable
+     * @return boolean
+     */
+    public static boolean createDeliverable(Deliverable deliverable) {
+        PreparedStatement sqlStatement;
+        int intResult;
+
+        String query = "INSERT INTO `deliverables`(`category_id`, `deliverable_name`, `deliverable_detail`, `deliverable_start`, `deliverable_end`, `status_id`) VALUES (?,?,?,?,?,?)";
+
+        try {
+            sqlStatement = connectToDatabase().prepareStatement(query);
+
+            sqlStatement.setInt(1, deliverable.intCategory());
+            sqlStatement.setString(2, deliverable.strName());
+            sqlStatement.setString(3, deliverable.strDetail());
+            sqlStatement.setDate(4, deliverable.dtStart());
+            sqlStatement.setDate(5, deliverable.dtEnd());
+            sqlStatement.setInt(6, deliverable.intStatus());
+
+            intResult = sqlStatement.executeUpdate();
+
+            if (intResult > 0) {
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+
+    /**
+     * @param deliverable
+     * @param intId
+     * @return boolean
+     */
+    public static boolean updateDeliverable(Deliverable deliverable, int intId) {
+        PreparedStatement sqlStatement;
+        int intResult;
+
+        String query = "UPDATE `deliverables` SET `category_id`=?, `deliverable_name`=?, `deliverable_detail`=?,`deliverable_end`=?,`status_id`=? WHERE `deliverable_id` =?";
+
+        try {
+            sqlStatement = connectToDatabase().prepareStatement(query);
+
+            sqlStatement.setInt(1, deliverable.intCategory());
+            sqlStatement.setString(2, deliverable.strName());
+            sqlStatement.setString(3, deliverable.strDetail());
+            sqlStatement.setDate(4, deliverable.dtEnd());
+            sqlStatement.setInt(5, deliverable.intStatus());
+            sqlStatement.setInt(6, intId);
+
+            intResult = sqlStatement.executeUpdate();
+
+            if (intResult > 0) {
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+
+    /**
+     * @param intId
+     * @return boolean
+     */
+    public static boolean deleteDeliverable(int intId) {
+        PreparedStatement sqlStatement;
+        int intResult;
+
+        String query = "DELETE FROM `deliverables` WHERE `deliverable_id` =?";
+
+        try {
+            sqlStatement = connectToDatabase().prepareStatement(query);
+
+            sqlStatement.setInt(1, intId);
+
+            intResult = sqlStatement.executeUpdate();
+
+            if (intResult > 0) {
+                return true;
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+
+    }
+
+    /**
+     * @param strName
+     * @return int
+     */
+    public static int findDeliverableId(String strName) {
+        int intId;
+        PreparedStatement sqlStatement;
+        ResultSet sqlResult;
+
+        String query = "SELECT * FROM `deliverables` WHERE `deliverable_name` =?";
+
+        try {
+            sqlStatement = connectToDatabase().prepareStatement(query);
+
+            sqlStatement.setString(1, strName);
+
+            sqlResult = sqlStatement.executeQuery();
+
+            while (sqlResult.next()) {
+                intId = sqlResult.getInt("deliverable_id");
+                return intId;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        // idk what to return if this method fails
+        return -1;
+    }
+
+}
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -6,33 +6,24 @@ import java.sql.SQLException;
 
 import javax.swing.table.DefaultTableModel;
 
+import model.Deliverable;
 import model.Session;
 
-public class MainController extends DatabaseConnection {
+public class MainController extends DeliverableOperations {
     // TODO: will contain functions shared between both frames,
     // fetch table
-    // also contain functions only for the main veiw frame.. like selectable table... implementation might vary on the view
+    // also contain functions only for the main veiw frame.. like selectable
+    // table... implementation might vary on the view
     private static Session session;
-    private static int intAction = 1;
     private static int intUser;
     private static String sqlDate = "NOW()";
     private static String strDetail = "User ";
     private static String strName = "";
     private static String strLevel;
 
-    // TODO: functions for deliverables
-    // creating deliverables, 
-    // editing deliverable status, 
-    // editing deliverable name or detail
-    // editing deliverable category
-    // deleting deliverable
-
-    // FUNCTIONS HERE WILL GET SHIT FROM VIEW, THEN EXECUTE FROM DELIVERABLE RECORD
-    // ANDUN YNUG SQL SHIT WAG KA MALITO
-
-
-    //INSERT INTO deliverables(category_id, deliverable_name, deliverable_start, status_id) VALUES (1,"Finish REAME",CURDATE(), 2);
-
+    // differientiate anong part ng deliverable ang iedit
+    // generate deliverable shits here
+    // Deliverable deliverable = new Deliverable(0, null, null, null, null, 0);
     
     
 }
